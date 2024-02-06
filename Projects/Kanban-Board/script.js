421cdfc74ccb10cd869bb15b711f0bf0f2b849cf
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2024-02-06 23:03:26 +0530

    remove button logic

--- Projects/Kanban-Board/script.js
+++ Projects/Kanban-Board/script.js
@@ -3,14 +3,27 @@ const modalCont = document.querySelector(".modal-cont");
 const mainTicketContainer = document.querySelector(".main-ticket-cont");
 const textArea = document.querySelector(".textarea-cont");
 const allPriorityColor = document.querySelectorAll(".priority-color");
+const removeBtn = document.querySelector(".remove-btn");
 
 let addTaskFlag = false;
+let removeTaskFlag = false;
 let modalPriorityColor = "black";
 const cleanUpColorSelction = () => {
   allPriorityColor.forEach((currElem) => {
     currElem.classList.remove("active-status-color");
   });
 };
+
+removeBtn.addEventListener("click", () => {
+  removeTaskFlag = !removeTaskFlag;
+  if (removeTaskFlag == true) {
+    window.alert("Delete Button Has Been Activated");
+    removeBtn.style.color = "red";
+  } else {
+    window.alert("Delete Button Has Been Deactivated");
+    removeBtn.style.color = "white";
+  }
+});
 
 addBtn.addEventListener("click", () => {
   addTaskFlag = !addTaskFlag;
@@ -51,4 +64,13 @@ const createTicket = (ticketInfo) => {
     <i class="fa-solid fa-lock"></i>
   </div>`;
   mainTicketContainer.appendChild(ticketCont);
+  handleRemove(ticketCont);
+};
+
+const handleRemove = (ticket) => {
+  ticket.addEventListener("click", () => {
+    if (removeTaskFlag === true) {
+      ticket.remove();
+    }
+  });
 };
