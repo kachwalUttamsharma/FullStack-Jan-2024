84bec22ef4e9573c9c9afa0d9bcf48e6423bd919
kachwalUttamsharma <kachwaluttamsharma@gmail.com>
2024-03-28 23:26:33 +0530

    loader added on movies

--- Projects/imdb_clone/src/Components/Movies.js
+++ Projects/imdb_clone/src/Components/Movies.js
@@ -5,11 +5,19 @@ import Pagination from "./Pagination";
 const Movies = () => {
   const [movies, setMovies] = useState([]);
   const [page, setPage] = useState(1);
+  const [loader, setLoader] = useState(false);
 
   useEffect(() => {
+    setLoader(true);
     GetTrendingMovies(page)
-      .then((data) => setMovies(data))
-      .catch((err) => console.error(err));
+      .then((data) => {
+        setMovies(data);
+        setLoader(false);
+      })
+      .catch((err) => {
+        console.error(err);
+        setLoader(false);
+      });
   }, [page]);
 
   const loadNextPageMovies = () => {
@@ -23,30 +31,38 @@ const Movies = () => {
   };
   return (
     <div>
-      <div className="text-2xl mb-8 font-bold text-center">Trending Movies</div>
-      <div className="flex flex-wrap">
-        {movies &&
-          movies?.map((movie) => {
-            return (
-              <div
-                key={movie?.id}
-                className="w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-4 hover:scale-110 duration-300 "
-                style={{
-                  backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie?.poster_path})`,
-                }}
-              >
-                <div className="text-white text-center font-bold bg-gray-900 w-full bg-opacity-50">
-                  {movie?.title}
-                </div>
-              </div>
-            );
-          })}
-      </div>
-      <Pagination
-        onNext={loadNextPageMovies}
-        onPrev={loadPreviousPageMovies}
-        currPage={page}
-      />
+      {loader ? (
+        <div>Loading</div>
+      ) : (
+        <>
+          <div className="text-2xl mb-8 font-bold text-center">
+            Trending Movies
+          </div>
+          <div className="flex flex-wrap">
+            {movies &&
+              movies?.map((movie) => {
+                return (
+                  <div
+                    key={movie?.id}
+                    className="w-[160px] h-[30vh] bg-center bg-cover rounded-xl m-4 hover:scale-110 duration-300 "
+                    style={{
+                      backgroundImage: `url(https://image.tmdb.org/t/p/original/t/p/w500/${movie?.poster_path})`,
+                    }}
+                  >
+                    <div className="text-white text-center font-bold bg-gray-900 w-full bg-opacity-50">
+                      {movie?.title}
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+          <Pagination
+            onNext={loadNextPageMovies}
+            onPrev={loadPreviousPageMovies}
+            currPage={page}
+          />
+        </>
+      )}
     </div>
   );
 };
